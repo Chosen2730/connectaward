6290316a0c6ca856a0400ca475b143e4fd535b20
Chosen2730 <70141158+Chosen2730@users.noreply.github.com>
2022-06-19 22:29:24 +0100

    added the navbar

--- src/context.js
+++ src/context.js
@@ -2,7 +2,7 @@ import { useContext, useState, createContext } from "react";
 
 const AppContext = createContext();
 const AppProvider = ({ children }) => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isNavOpen, setIsNavOpen] = useState(true);
   return (
     <AppContext.Provider value={{ isNavOpen, setIsNavOpen }}>
       {children}
